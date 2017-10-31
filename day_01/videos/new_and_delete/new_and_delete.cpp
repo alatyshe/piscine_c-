d332b10d212c3003a88726efc905cd8cb13a671d
alatyshe <la07121610@gmail.com>
2017-10-31 21:07:25 +0200

    add day_01

---
+++ day_01/videos/new_and_delete/new_and_delete.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+
+class Student
+{
+private:
+	std::string _login;
+
+public:
+	Student(std::string login) : _login(login)
+	{
+		std::cout << "Student " << this->_login << " is born" << std::endl;
+	}
+	~Student()
+	{
+		std::cout << "Student " << this->_login << " died" << std::endl;
+	}
+	
+};
+
+int			main()
+{
+	Student		bob = Student("Bfubar");
+
+	Student*	jim = new Student("Jfubar");
+
+
+	//	Do some stuff here
+
+	delete jim; // jim is destroyed;
+
+	return (0); // bob is destroyed;
+}
